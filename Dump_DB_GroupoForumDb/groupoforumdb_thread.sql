3da946f95a5c8b96842f903bf73092ae55deb2c8
Axelle <ellvorfilia.axelle@gmail.com>
2021-10-10 18:04:47 +0200

    Création d'un dump de la base de données

---
+++ Dump_DB_GroupoForumDb/groupoforumdb_thread.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE  IF NOT EXISTS `groupoforumdb` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `groupoforumdb`;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: groupoforumdb
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `thread`
+--
+
+DROP TABLE IF EXISTS `thread`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `thread` (
+  `id` smallint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` smallint unsigned NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_id_thread` (`user_id`),
+  CONSTRAINT `fk_user_id_thread` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `thread`
+--
+
+LOCK TABLES `thread` WRITE;
+/*!40000 ALTER TABLE `thread` DISABLE KEYS */;
+INSERT INTO `thread` VALUES (8,6,'Je suis un message a afficher','Je suis le contenu de ce message','2021-10-10 15:18:45'),(9,6,'Je suis un nouveau Message','Je suis un message d\'un utilisateur','2021-10-10 15:55:57');
+/*!40000 ALTER TABLE `thread` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-10 17:56:58
--- readMe.txt
+++ readMe.txt
@@ -1,9 +1,14 @@
-Parcours Développement Web OpenClassrooms - Projet n°
+Parcours Développement Web OpenClassrooms - Projet n°7
 
 Créez un réseau social d'entreprise
 
-Projet démarré le 27 Septembre 2021
+Project start 27 September 2021
 
-Lien Repository Github : https://github.com/Axelle30/AxelleRichard_7_27092021.git
+Repository Github Link : https://github.com/Axelle30/AxelleRichard_7_27092021.git
+
+Run "npm run serve" in groupomania-client folder to lauch VueJs app (port 8081)
+Run "nodemon server" in API folder to launch express app (port 3000)
+
+There is a dump of database in Dump_DB_GroupoForumDb (generated with MySql Workbench)
 
 Axelle Richard
